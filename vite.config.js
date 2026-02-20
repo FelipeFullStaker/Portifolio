fdaa5a469fe526e657f2d19da44fe2108178d882
Feguto <lipelipeguto@gmail.com>
2026-02-20 16:30:53 -0300

    lapidando site

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portifolio/', // 👈 NOME EXATO DO SEU REPOSITÓRIO!
+  base: 'https://github.com/FelipeFullStaker/Portifolio.git', // 👈 NOME EXATO DO SEU REPOSITÓRIO!
 })
